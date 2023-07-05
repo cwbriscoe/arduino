d6ee2b4699c526832d371033ded8a4cc6a969b94
cwbriscoe <cbriscoe658@yahoo.com>
2023-07-05 12:39:30 -0700

    wip

--- test/src/env.h
+++ test/src/env.h
@@ -5,8 +5,6 @@
 #ifndef ENV_H
 #define ENV_H
 
-#define DEBUG
-
 const byte MAX_THREADS = 7;       // max number ofr threads/tasks that can be added to the thread manager
 const byte ANALOG_MAX_BITS = 10;  // number of bits of data provided in analogRead()
 
